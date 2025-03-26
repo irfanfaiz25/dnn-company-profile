d988656fa1d356b52564b2df1fa9d54c61d10fe1
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-03-26 12:55:54 +0700

    feat: create order button in produk page

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Add this line
+    port: 5173,
+    hmr: {
+      host: "localhost",
+      clientPort: 5173,
+      overlay: true,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 });
